23dfa9e560645dc8200ea9c6be147d980b1828e4
Dylan <94193563+dbooth05@users.noreply.github.com>
2024-02-15 16:55:46 -0600

    Comments added

--- api_utils.py
+++ api_utils.py
@@ -9,7 +9,7 @@ s = serial.Serial(
 )
 
 class ControlMode(Enum):
-    PercentOutput = 0
+    PercentOutput = 0		
     Position = 1
     Velocity = 2
     Current = 3
@@ -22,8 +22,10 @@ class ControlMode(Enum):
 
 # root function to generate messages
 def send_msg(func_num, motor_num, *argv):
+    # parts is the final message, struct.pack turns message into bytes, pack enum (i) and then append motor number in following
     parts = [struct.pack('i', func_num)]
     parts.append(struct.pack('i', motor_num))
+    # adding additional arguments
     for arg in argv:
         if isinstance(arg, float):
             parts.append(struct.pack('d', arg))
@@ -31,19 +33,25 @@ def send_msg(func_num, motor_num, *argv):
             parts.append(struct.pack('i', arg))
         if isinstance(arg, ControlMode):
             parts.append(struct.pack('i', arg.value))
-    msg = b''
+    msg = b'' # msg is empty byte literal then read msg byes into it
     for part in parts:
         msg = msg + part
+    # random buffer variable
     x = b''
+
+    # xon message, reads till a xon
     while x != b'\x13':
         x = s.read(1)
     s.write(msg)
+    # listening for xoff
     while x != b'\x11':
         x = s.read(1)
-
+	    
+# template message
 def set(mode: ControlMode, outputValue: float):
 	send_msg(0, 0, ControlMode(mode), float(outputValue))
 
+# testing spins motors up and down quickly
 while True:
     for i in range(101):
         set(ControlMode.PercentOutput, float(i/200))
